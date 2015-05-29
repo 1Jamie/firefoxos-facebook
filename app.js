a93dbc2aadf7e0e3e4d0c7581dbfa6a8fe56bdf5
1Jamie <charja13@live.com>
2015-05-29 00:35:00 -0400

    updated setup

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 window.addEventListener("load", function() {
   console.log("facebook hi!");
+GET /v2.3/me HTTP/1.1
+Host: graph.facebook.com
 });
+window.addEventListener("load", function(){
+window.fbAsyncInit = function() {
+        FB.init({
+          status     : true,
+          appId      : '1432144570413455',
+          xfbml      : true,
+          version    : 'v2.3'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));}
+
+
