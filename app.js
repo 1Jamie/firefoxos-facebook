666f3e739b79c683ed9779c228f77f488af2ba35
1Jamie <charja13@live.com>
2015-05-30 17:51:19 -0400

    some more added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
   console.log("facebook hi!");
-GET /v2.3/me HTTP/1.1
-Host: graph.facebook.com
+//GET /v2.3/me HTTP/1.1
+//Host: graph.facebook.com
 });
 
     /**
@@ -38,20 +38,31 @@ Host: graph.facebook.com
       document.body.appendChild(js);
      
     // Call the facebook init.
-    FB.init({
-      appId      : '1432144570413455',
-      status     : true,
-      xfbml      : true,
-      version    : 'v2.3',
-    });
+    window.addEventListener("load", function() {
+      FB.init({
+        appId      : '1432144570413455',
+        status     : true,
+        xfbml      : true,
+        version    : 'v2.3'
+      });
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+      //statusChangeCallback(response)
          
-    // Try to login, trigger callback with results.
-    FB.login(response => {
-      statusChangeCallback(response);
-    });
+      // Try to login, trigger callback with results.
+      FB.login(response => {
+        statusChangeCallback(response);
      
-    // Check the login status, trigger callback with results.
-    FB.getLoginStatus(response => {
-      statusChangeCallback(response);
+        // Check the login status, trigger callback with results.
+        FB.getLoginStatus(response => {
+          statusChangeCallback(response);
+        });
+      });
     });
-     
+    
